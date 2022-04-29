ce7b31b8c97a2ee0e5a80a0f950efd51a994c5b5
Taran van Groenigen <tvangroenigen@outlook.com>
2022-04-30 00:16:47 +0200

    Mostly working timer

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <video
+      ref="video"
       id="video"
       autoplay
       muted
@@ -8,17 +9,44 @@
   </div>
 </template>
 
+<style lang="scss">
+#video {
+  opacity: 0;
+  position: absolute;
+  pointer-events: none;
+}
+</style>
+
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
+import { Watch } from 'vue-property-decorator'
 
-@Options({})
+@Options({
+  props: {
+    duration: {
+      type: Number,
+      required: true,
+      validator: (nr: number) => nr > 0
+    },
+    paused: {
+      type: Boolean,
+      default: () => false
+    }
+  }
+})
 export default class Timer extends Vue {
   canvas: HTMLCanvasElement | null = null;
   ctx: CanvasRenderingContext2D | null = null;
+  video: HTMLVideoElement | null = null
+  lastTick = 0
+  elapsed = 0
+
+  paused!: boolean
+  duration!: number
 
   public mounted() {
     const canvas = document.createElement('canvas')
-    const video = document.getElementById('video') as HTMLVideoElement
+    const video = this.$refs.video as HTMLVideoElement
     const ctx = canvas.getContext('2d')
     if (ctx && video) {
       ctx.font = '50px Roboto'
@@ -30,34 +58,83 @@ export default class Timer extends Vue {
 
       this.canvas = canvas
       this.ctx = ctx
-      this.requestFrame()
+      this.video = video
+      this.start()
     }
   }
 
+  public requestPip() {
+    this.video?.requestPictureInPicture()
+  }
+
+  @Watch('duration')
+  public onDurationChanged() {
+    this.elapsed = 0
+  }
+
+  @Watch('paused')
+  public onPausedChanged(paused: boolean) {
+    if (!paused) {
+      this.start()
+    }
+  }
+
+  public reset() {
+    this.elapsed = 0
+    if (this.paused) {
+      this.start()
+    }
+  }
+
+  private start() {
+    this.lastTick = Date.now()
+    this.requestFrame()
+  }
+
   private clear() {
-    if (this.ctx && this.canvas) {
-      this.ctx.fillStyle = 'white'
+    if (this.ctx && this.canvas && this.video) {
+      const color = getComputedStyle(this.video).getPropertyValue('--v-theme-background')
+      this.ctx.fillStyle = `rgb(${color})`
       this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
     }
   }
 
   private displayText() {
-    if (this.ctx && this.canvas) {
-      this.ctx.fillStyle = 'black'
+    if (this.ctx && this.canvas && this.video) {
+      const color = getComputedStyle(this.video).getPropertyValue('--v-theme-on-surface')
+      this.ctx.fillStyle = `rgb(${color})`
 
-      const time = new Date().toLocaleTimeString()
-      this.ctx.fillText(time, this.canvas.width / 2, this.canvas.height / 2)
+      const remaining = new Date(this.timeRemaining)
+      const minutes = remaining.getMinutes().toString().padStart(2, '0')
+      const seconds = remaining.getSeconds().toString().padStart(2, '0')
+      this.ctx.fillText(`${minutes}:${seconds}`, this.canvas.width / 2, this.canvas.height / 2)
+    }
+  }
+
+  private updateTimer() {
+    if (!this.paused && this.timeRemaining > 0) {
+      const delta = Date.now() - this.lastTick
+      this.lastTick = Date.now()
+      this.elapsed += delta
     }
   }
 
   private anim() {
+    this.updateTimer()
     this.clear()
     this.displayText()
-    this.requestFrame()
+
+    if (!this.paused) {
+      this.requestFrame()
+    }
   }
 
   private requestFrame() {
     requestAnimationFrame(() => this.anim())
+  }
+
+  private get timeRemaining() {
+    return Math.max(this.duration - this.elapsed, 0)
   }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
-  <timer />
+  <timer
+    ref="timer"
+    :duration="60000"
+    :paused="this.paused"
+  />
+  <v-btn v-on:click="this.$refs.timer.requestPip()">PIP</v-btn>
+  <v-btn v-on:click="this.$refs.timer.reset()">Restart</v-btn>
+  <v-switch
+    v-model="this.paused"
+    label="Paused"
+  />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { Vue, Options } from 'vue-class-component'
 
 // Components
 import Timer from '../components/Timer.vue'
 
-export default defineComponent({
-  name: 'HomeView',
-
+@Options({
   components: {
     Timer
   }
 })
+export default class HomeView extends Vue {
+  paused = false
+}
 </script>
